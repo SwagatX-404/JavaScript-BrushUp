c3f4a0648e7697d0cc31ebeb701e6cb5508f1eea
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-02-02 19:40:25 +0530

    if == & ===

---
+++ 05_ControlFlow/if.js
@@ -0,0 +1,13 @@
+if(2=="2")// loose equal
+{
+    console.log("true");
+    
+}
+if(2==="2")//=== strict equal
+    {
+        console.log("true");   
+    }
+    else{
+        console.log("false");
+        
+    }
